e911bcb062cb877ba69dbf5e47c720a55b4083fe
Bouzaien Malek <malek_ensi@yahoo.fr>
2025-10-18 11:04:38 +0100

    Projet Java Maven initial

---
+++ tp1/src/main/java/com/example/tp/App.java
@@ -0,0 +1,11 @@
+package com.example.tp;
+
+public class App {
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("3 + 4 = " + addition(3, 4));
+    }
+}
