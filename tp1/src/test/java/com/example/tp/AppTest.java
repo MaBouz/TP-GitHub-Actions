74dc17c1895fde04fc7c51b2da75546dc057c054
MaBouz <74194221+MaBouz@users.noreply.github.com>
2025-10-18 12:28:51 +0100

    Update AppTest.java

--- tp1/src/test/java/com/example/tp/AppTest.java
+++ tp1/src/test/java/com/example/tp/AppTest.java
@@ -1,11 +1,1 @@
-package com.example.tp;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
-public class AppTest {
-    @Test
-    public void testAddition() {
-        assertEquals(7, App.addition(3, 4));
-    }
-}
